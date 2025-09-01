6272ff5f8c9616b40fbc3a107a9259604e790b38
lakhatekno <lakhatekno2514@gmail.com>
2025-09-01 18:19:20 +0700

    [Admin][Dashboard] Modularization attendance log section

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,38 +1,25 @@
-import AttendanceLogFilter from '@/components/features/admin-dashboard/AttendanceLogFilter.component';
-import LogTable from '@/components/features/admin-dashboard/LogTable.component';
+'use client'
+
+import AttendanceLog from '@/components/features/admin-dashboard/AttendanceLog.component';
 import Clock from '@/components/features/admin-dashboard/Clock.component';
 
-import { TbNotes } from 'react-icons/tb';
-import { RiFileExcel2Line } from 'react-icons/ri';
 import { MdOutlineQrCodeScanner } from 'react-icons/md';
 
 export default function AdminPage() {
 	return (
 		<div className="flex flex-col gap-8">
+			{/* Top Section */}
 			<section className="bg-white shadow rounded-xl p-8">
 				<Clock />
-				<button className='flex items-center gap-2 px-4 py-2 bg-indigo-900 text-white font-semibold rounded-lg mt-8'>
+
+				{/* QR Button */}
+				<button className="flex items-center gap-2 px-4 py-2 bg-indigo-900 text-white font-semibold rounded-lg mt-8">
 					<MdOutlineQrCodeScanner />
 					Buat QR
 				</button>
 			</section>
-			<section className="bg-white shadow rounded-xl p-8">
-				<div className="flex gap-4 items-center mb-8 text-indigo-900">
-					<TbNotes className="scale-150" />
-					<h2 className="font-bold text-xl">Log Check In & Check Out</h2>
-				</div>
-				<div className="flex justify-between">
-					<AttendanceLogFilter />
-					<button
-						className="bg-green-600 flex gap-2 font-medium h-fit w-fit px-4 py-2 items-center rounded-lg text-sm text-slate-100"
-						disabled
-					>
-						<RiFileExcel2Line className="scale-125" />
-						Ekspor Excel
-					</button>
-				</div>
-				<LogTable />
-			</section>
+			{/* Attendance Log */}
+			<AttendanceLog />
 		</div>
 	);
 }
---
+++ src/components/features/admin-dashboard/AttendanceLog.component.tsx
@@ -0,0 +1,43 @@
+import { MdOutlineNoteAdd } from 'react-icons/md';
+import { TbNotes } from 'react-icons/tb';
+import { RiFileExcel2Line } from 'react-icons/ri';
+import AttendanceLogFilter from './AttendanceLogFilter.component';
+import LogTable from './LogTable.component';
+import BaseModal from '@/components/ui/modals/base.modal';
+import { useAttendanceLogStore } from '@/store/attendance-log.store';
+import AttendanceModal from '@/components/ui/modals/attendance.modal';
+
+export default function AttendanceLog() {
+	const { manualRecordModal, setManualRecordModal } = useAttendanceLogStore();
+	return (
+		<section className="bg-white shadow rounded-xl p-8">
+			{/* Section Header */}
+			<div className="flex gap-4 items-center mb-8 text-indigo-900">
+				<TbNotes className="scale-150" />
+				<h2 className="font-bold text-xl">Log Check In & Check Out</h2>
+				<button
+					className="rounded-lg border border-indigo-900 bg-white hover:bg-indigo-900 px-4 py-2 w-fit h-fit text-indigo-900 hover:text-white transition-all font-semibold cursor-pointer flex gap-2 items-center text-xs"
+					onClick={setManualRecordModal}
+				>
+					<MdOutlineNoteAdd />
+					Catat Kehadiran Manual
+				</button>
+			</div>
+
+			{/* Filter and Export button */}
+			<div className="flex justify-between">
+				<AttendanceLogFilter />
+				<button
+					className="bg-green-600 flex gap-2 font-medium h-fit w-fit px-4 py-2 items-center rounded-lg text-sm text-slate-100"
+					disabled
+				>
+					<RiFileExcel2Line className="scale-125" />
+					Ekspor Excel
+				</button>
+			</div>
+
+			{/* Table */}
+			<LogTable />
+		</section>
+	);
+}
