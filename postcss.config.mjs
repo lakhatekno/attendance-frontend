0ef196ad4dd773ce067ff9a28309544453c9c650
lakhatekno <lakhatekno2514@gmail.com>
2025-10-02 19:46:59 +0700

    [Tailwind] Edit configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,5 @@
 const config = {
 	plugins: ['@tailwindcss/postcss'],
-	theme: {
-		extend: {
-			colors: {
-				'primary': '#2892D7',
-				'accent': '#F17F29',
-			},
-		},
-	},
 };
 
 export default config;
