22a4d44b5310168dbaf39c829079d63bdfa84fbb
Muhammad Islakha <lakhatekno2514@gmail.com>
2025-10-03 02:17:33 +0700

    [Auth][API] fix wrong address

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -2,7 +2,7 @@ import api from './axios';
 import { Login } from '@/model';
 
 export async function login(data: Login): Promise<any> {
-	const response = await api.post('/login', {
+	const response = await api.post('/auth/login', {
 		username: data.username,
 		password: data.password,
 	});
