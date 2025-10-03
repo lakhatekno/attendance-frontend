0e85b5fab37c62b037280ecfd9747f5a2fdda079
Muhammad Islakha <lakhatekno2514@gmail.com>
2025-10-04 00:07:15 +0700

    [Libs] Time formatter from type Date

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -6,3 +6,15 @@ export const formatDate = (date: string): string => {
 		day: 'numeric',
 	});
 };
+
+export const formatTime = (date: string): string => {
+  const d = new Date(date);
+  const formatter = new Intl.DateTimeFormat('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hourCycle: 'h23',
+    timeZone: 'Asia/Jakarta'
+  });
+
+  return formatter.format(d);
+}
