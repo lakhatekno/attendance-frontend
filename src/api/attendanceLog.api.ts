207e84053a2ddc3b1b9945a3ef76fa8637678d43
Muhammad Islakha <lakhatekno2514@gmail.com>
2025-10-04 17:21:52 +0700

    [Admin][Dashboard][Attendance Log] API update record

--- src/api/attendanceLog.api.ts
+++ src/api/attendanceLog.api.ts
@@ -1,5 +1,6 @@
 import { AuthStore } from '@/store/auth.store';
 import api from './axios';
+import { AttendanceLog } from '@/model';
 
 export async function getAllAttendanceLogs() {
 	const token = AuthStore.getState().accessToken;
@@ -10,4 +11,18 @@ export async function getAllAttendanceLogs() {
 	});
 
 	return response.data;
+}
+
+export async function updateAttendanceLog(data: AttendanceLog) {
+  const token = AuthStore.getState().accessToken;
+  const response = await api.put('/attendance-logs', {
+    data
+  },
+  {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    }
+  });
+
+  return response.data;
 }
