0836bb23ac0237967a7322a1c6ec8fc80586628c
lakhatekno <lakhatekno2514@gmail.com>
2025-10-15 19:52:40 +0700

    [Admin][Shift] State open addShiftmodal. prettier.

--- src/components/features/shift-management/HeadSection.component.tsx
+++ src/components/features/shift-management/HeadSection.component.tsx
@@ -1,7 +1,11 @@
-import { RiAddFill } from "react-icons/ri";
-import { GrScheduleNew } from "react-icons/gr";
+import { RiAddFill } from 'react-icons/ri';
+import { GrScheduleNew } from 'react-icons/gr';
+import AddShiftModal from '@/components/ui/modals/addShift.modal';
+
+import { useShiftStore } from '@/store/shift.store';
 
 export default function HeadSection() {
+	const { setOpenModal } = useShiftStore();
 	return (
 		<div className="flex justify-between items-center mb-8">
 			<div className="flex gap-4 items-center text-indigo-900">
@@ -10,13 +14,15 @@ export default function HeadSection() {
 			</div>
 			<div className="flex gap-2 items-center w-fit">
 				<button
-					onClick={() => {}}
+					onClick={setOpenModal}
 					className="bg-green-600 flex gap-2 font-medium h-fit w-fit px-4 py-2 items-center rounded-lg text-sm text-slate-100 cursor-pointer"
 				>
 					<RiAddFill className="scale-125" />
 					Tambah Shift
 				</button>
 			</div>
+
+			<AddShiftModal />
 		</div>
 	);
 }
