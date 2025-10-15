87cdbf342b067dc8a18c12b3922ee3d31662651d
lakhatekno <lakhatekno2514@gmail.com>
2025-10-15 19:50:38 +0700

    [Admin][Shift] State management, getAllShift, createShift

---
+++ src/store/shift.store.ts
@@ -0,0 +1,28 @@
+import { createShift, getAllActiveShift } from '@/api/shift.api';
+import { ShiftBody, ShiftState } from '@/model';
+import { create } from 'zustand';
+
+export const useShiftStore = create<ShiftState>((set, get) => ({
+	openModal: false,
+	setOpenModal: () => set({ openModal: !get().openModal }),
+	shiftData: [],
+	message: { error: false, message: '' },
+	setShiftData: async () => {
+		try {
+			const response = await getAllActiveShift();
+			set({ shiftData: response });
+		} catch (error) {
+			console.log(error);
+		}
+	},
+	createShift: async (data: ShiftBody) => {
+		try {
+			await createShift(data);
+			set({ message: { error: false, message: 'Shift berhasil ditambahkan' } });
+      get().setOpenModal();
+			get().setShiftData();
+		} catch (error: any) {
+			set({ message: { error: true, message: error.response?.data?.message || 'Gagal menambahkan shift' } });
+		}
+	}
+}));
