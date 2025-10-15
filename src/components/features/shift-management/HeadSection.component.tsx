a3cf8b6a3ab8725368a1142c3ad0108ede1a152e
lakhatekno <lakhatekno2514@gmail.com>
2025-10-15 22:09:19 +0700

    [Admin][Shift][Inactive Shift] Interface: table, toggle

--- src/components/features/shift-management/HeadSection.component.tsx
+++ src/components/features/shift-management/HeadSection.component.tsx
@@ -1,11 +1,12 @@
 import { RiAddFill } from 'react-icons/ri';
 import { GrScheduleNew } from 'react-icons/gr';
 import AddShiftModal from '@/components/ui/modals/addShift.modal';
+import { FaRegEye } from 'react-icons/fa';
 
 import { useShiftStore } from '@/store/shift.store';
 
 export default function HeadSection() {
-	const { setOpenModal } = useShiftStore();
+	const { setOpenModal, setShowInactive } = useShiftStore();
 	return (
 		<div className="flex justify-between items-center mb-8">
 			<div className="flex gap-4 items-center text-indigo-900">
@@ -13,6 +14,13 @@ export default function HeadSection() {
 				<h2 className="font-bold text-xl">Pengaturan Shift</h2>
 			</div>
 			<div className="flex gap-2 items-center w-fit">
+				<button
+					onClick={setShowInactive}
+					className="bg-blue-400 hover:bg-blue-500 flex gap-2 font-medium h-fit w-fit px-4 py-2 items-center rounded-lg text-sm text-slate-100 cursor-pointer"
+				>
+					<FaRegEye className="scale-125" />
+					Shift Nonaktif
+				</button>
 				<button
 					onClick={setOpenModal}
 					className="bg-green-600 flex gap-2 font-medium h-fit w-fit px-4 py-2 items-center rounded-lg text-sm text-slate-100 cursor-pointer"
--- src/components/features/shift-management/TableListOfShifts.component.tsx
+++ src/components/features/shift-management/TableListOfShifts.component.tsx
@@ -1,52 +1,96 @@
-import { ShiftInterface } from "@/model";
-import { useShiftStore } from "@/store/shift.store";
-import { formatTime } from "@/utils/date";
-import { useEffect } from "react";
-import { BiPencil, BiTrash } from "react-icons/bi";
+import { ShiftInterface } from '@/model';
+import { useShiftStore } from '@/store/shift.store';
+import { formatTime } from '@/utils/date';
+import { useEffect } from 'react';
+import { BiTrash } from 'react-icons/bi';
 
 export function TableListOfShifts() {
-  const { shiftData, setShiftData, inactivateShift } = useShiftStore();
+	const { showInactive, shiftData, inactiveShiftData, setShiftData, setInactiveShiftData, inactivateShift, activateShift } =
+		useShiftStore();
 
 	const handlerInactivateShift = (id: number) => {
 		inactivateShift(id);
-	}
-  useEffect(() => {
-    setShiftData();
-  }, [setShiftData]);
+	};
+
+	const handlerActivateShift = (id: number) => {
+		activateShift(id);
+	};
+	useEffect(() => {
+		setShiftData();
+		setInactiveShiftData();
+	}, []);
 
 	return (
-		<table className="table-auto border-collapse w-full text-sm">
-			<thead>
-				<tr className="text-slate-900 font-medium text-left">
-					<th className="border-b p-2">Nama</th>
-					<th className="border-b p-2">Jam Mulai</th>
-					<th className="border-b p-2">Jam Selesai</th>
-					<th className="border-b p-2 w-32">Cross Day</th>
-				</tr>
-			</thead>
-			<tbody>
-				{shiftData.map((shift: ShiftInterface, index: number) => (
-					<tr
-						key={shift.id}
-						className={`hover:bg-gray-200 text-xs border-b border-b-gray-200 ${
-							index % 2 !== 0 ? 'bg-gray-100' : 'bg-white'
-						}`}
-					>
-						<td className="p-2">{shift.name}</td>
-						<td className="p-2">{formatTime(shift.shift_start)}</td>
-						<td className="p-2">{formatTime(shift.shift_end)}</td>
-						<td className="p-2">{shift.cross_day ? 'Ya' : 'Tidak'}</td>
-						<td className="p-2 flex gap-6 text-slate-600">
-							<button
-								className="flex items-center gap-1 hover:text-red-500 transition-colors cursor-pointer"
-								onClick={() => handlerInactivateShift(shift.id)}
-							>
-								<BiTrash /> Nonaktifkan
-							</button>
-						</td>
+		<>
+			<table className="table-fixed border-collapse w-full text-sm">
+				<thead>
+					<tr className="text-slate-900 font-medium text-left">
+						<th className="border-b p-2">Nama</th>
+						<th className="border-b p-2">Jam Mulai</th>
+						<th className="border-b p-2">Jam Selesai</th>
+						<th className="border-b p-2 w-32">Cross Day</th>
 					</tr>
-				))}
-			</tbody>
-		</table>
+				</thead>
+				<tbody>
+					{shiftData.map((shift: ShiftInterface, index: number) => (
+						<tr
+							key={shift.id}
+							className={`hover:bg-gray-200 text-xs border-b border-b-gray-200 ${index % 2 !== 0 ? 'bg-gray-100' : 'bg-white'}`}
+						>
+							<td className="p-2">{shift.name}</td>
+							<td className="p-2">{formatTime(shift.shift_start)}</td>
+							<td className="p-2">{formatTime(shift.shift_end)}</td>
+							<td className="p-2">{shift.cross_day ? 'Ya' : 'Tidak'}</td>
+							<td className="p-2 flex gap-6 text-slate-600">
+								<button
+									className="flex items-center gap-1 hover:text-red-500 transition-colors cursor-pointer"
+									onClick={() => handlerInactivateShift(shift.id)}
+								>
+									<BiTrash /> Nonaktifkan
+								</button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+
+			{/* Inactive Shift */}
+			{showInactive && (
+				<div className='mt-8 flex flex-col'>
+					<p className='font-bold text-sm text-indigo-900'>Shift Nonaktif:</p>
+					<table className="table-fixed border-collapse w-full text-sm">
+						<thead>
+							<tr className="text-slate-900 font-medium text-left">
+								<th className="border-b p-2">Nama</th>
+								<th className="border-b p-2">Jam Mulai</th>
+								<th className="border-b p-2">Jam Selesai</th>
+								<th className="border-b p-2 w-32">Cross Day</th>
+							</tr>
+						</thead>
+						<tbody>
+							{inactiveShiftData.map((inactiveShift: ShiftInterface, index: number) => (
+								<tr
+									key={inactiveShift.id}
+									className={`hover:bg-gray-200 text-xs border-b border-b-gray-200 ${index % 2 !== 0 ? 'bg-gray-100' : 'bg-white'}`}
+								>
+									<td className="p-2">{inactiveShift.name}</td>
+									<td className="p-2">{formatTime(inactiveShift.shift_start)}</td>
+									<td className="p-2">{formatTime(inactiveShift.shift_end)}</td>
+									<td className="p-2">{inactiveShift.cross_day ? 'Ya' : 'Tidak'}</td>
+									<td className="p-2 flex gap-6 text-slate-600">
+										<button
+											className="flex items-center gap-1 hover:text-red-500 transition-colors cursor-pointer"
+											onClick={() => handlerActivateShift(inactiveShift.id)}
+										>
+											<BiTrash /> Aktifkan
+										</button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			)}
+		</>
 	);
 }
