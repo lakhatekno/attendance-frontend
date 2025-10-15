62482b00c3cf60c3c6274ad4605d97696d0973c2
lakhatekno <lakhatekno2514@gmail.com>
2025-10-15 22:08:05 +0700

    [Admin][Shift][Inactive Shift] API Get inactive shifts

--- src/api/shift.api.ts
+++ src/api/shift.api.ts
@@ -12,6 +12,17 @@ export async function getAllActiveShift() {
 	return response.data;
 }
 
+export async function getInactiveShift() {
+	const token = AuthStore.getState().accessToken;
+	const response = await api.get('/shifts/inactive', {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
+
+	return response.data;
+}
+
 export async function createShift(data: any) {
 	const token = AuthStore.getState().accessToken;
 	const response = await api.post('/shifts', data, {
@@ -26,6 +37,19 @@ export async function createShift(data: any) {
 export async function inactivateShift(id: number) {
 	const token = AuthStore.getState().accessToken;
 	const response = await api.put('/shifts/inactivate-shift', {
+		id: id,
+	}, {
+		headers: {
+			Authorization: `Bearer ${token}`
+		}
+	});
+
+	return response.status;
+}
+
+export async function activateShift(id: number) {
+	const token = AuthStore.getState().accessToken;
+	const response = await api.put('/shifts/activate-shift', {
 		id: id,
 	}, {
 		headers: {
