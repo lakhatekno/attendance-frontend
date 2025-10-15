d3ea4f71d336c8215e23dfdedf52a53dd4d3f085
lakhatekno <lakhatekno2514@gmail.com>
2025-10-15 19:50:07 +0700

    [Admin][Shift] Model for body request, model zustand state management

--- src/model/shift.model.ts
+++ src/model/shift.model.ts
@@ -6,3 +6,19 @@ export interface ShiftInterface {
   cross_day: boolean;
   active: boolean;
 }
+
+export interface ShiftBody {
+  name: string;
+  shiftStart: string;
+  shiftEnd: string;
+  crossDay: boolean;
+}
+
+export interface ShiftState {
+  shiftData: ShiftInterface[];
+  openModal: boolean;
+  message: { error: boolean; message: string };
+  setOpenModal: () => void;
+  setShiftData: () => void;
+  createShift: (data: ShiftBody) => void;
+}
