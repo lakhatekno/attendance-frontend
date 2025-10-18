aa1f0613ffd16c8acd0e49228fc37f2cc6382e97
lakhatekno <lakhatekno2514@gmail.com>
2025-10-19 04:56:11 +0700

    [Admin][Shift Assignment][Assignment Table] API fetch assigned data

---
+++ src/api/shiftManagement.api.ts
@@ -0,0 +1,23 @@
+import { AuthStore } from "@/store/auth.store";
+import api from "./axios";
+
+interface WindowAssignment {
+  month: number;
+  year: number;
+}
+
+export async function getAllAssignment(data: WindowAssignment) {
+  const token = AuthStore.getState().accessToken;
+  const response = await api.post('/shift-assignments/monthly', {
+    month: data.month + 1,
+    year: data.year,
+  }, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  });
+  
+  console.log(data);
+
+  return response.data;
+}
